b0b94a908c8b590577ab6e1da23f7e5fe464f6af
Stn-Labz, LLC. <pmei@stn-labz.com>
2026-09-22 18:07:36 -0700

    Add Contract snapshot qualification target

--- build.cmd
+++ build.cmd
@@ -15,8 +15,9 @@ if /i "%~1"=="test-contract" goto setup
 if /i "%~1"=="test-contract-consensus" goto setup
 if /i "%~1"=="test-contract-lineage" goto setup
 if /i "%~1"=="test-contract-state" goto setup
+if /i "%~1"=="test-contract-snapshot" goto setup
 if not "%~1"=="" (
-    echo Usage: build.cmd [clean^|test-contract^|test-contract-consensus^|test-contract-lineage^|test-contract-state]
+    echo Usage: build.cmd [clean^|test-contract^|test-contract-consensus^|test-contract-lineage^|test-contract-state^|test-contract-snapshot]
     exit /b 1
 )
 
@@ -59,6 +60,7 @@ if /i "%~1"=="test-contract" goto test_contract
 if /i "%~1"=="test-contract-consensus" goto test_contract_consensus
 if /i "%~1"=="test-contract-lineage" goto test_contract_lineage
 if /i "%~1"=="test-contract-state" goto test_contract_state
+if /i "%~1"=="test-contract-snapshot" goto test_contract_snapshot
 
 echo.
 echo STN Chain Windows x64 build
@@ -66,7 +68,7 @@ echo Compiler: Microsoft cl.exe
 echo Target:   %TARGET%
 echo.
 
-set "SOURCES= src\main.c  src\stn_address.c  src\stn_authority.c  src\stn_block.c  src\stn_chain.c  src\stn_contract.c  src\stn_contract_consensus.c  src\stn_contract_lineage.c  src\stn_contract_state.c  src\stn_contract_transaction.c  src\stn_fork.c  src\stn_identity.c  src\stn_sentinel_intelligence.c  src\stn_lifecycle.c  src\stn_mining.c  src\stn_node_service.c  src\stn_peer.c  src\stn_pending.c  src\stn_pow.c  src\stn_record.c  src\stn_replay.c  src\stn_rpc.c  src\stn_storage.c  src\stn_transaction.c  src\stn_validation.c  src\crypto\ed25519_donna\ed25519_provider.c  platforms\windows\stn_sha256.c  platforms\windows\stn_storage_windows.c  platforms\windows\stn_peer_windows.c  platforms\windows\stn_app_windows.c"
+set "SOURCES= src\main.c  src\stn_address.c  src\stn_authority.c  src\stn_block.c  src\stn_chain.c  src\stn_contract.c  src\stn_contract_consensus.c  src\stn_contract_lineage.c  src\stn_contract_state.c  src\stn_contract_snapshot.c  src\stn_contract_transaction.c  src\stn_fork.c  src\stn_identity.c  src\stn_sentinel_intelligence.c  src\stn_lifecycle.c  src\stn_mining.c  src\stn_node_service.c  src\stn_peer.c  src\stn_pending.c  src\stn_pow.c  src\stn_record.c  src\stn_replay.c  src\stn_rpc.c  src\stn_storage.c  src\stn_transaction.c  src\stn_validation.c  src\crypto\ed25519_donna\ed25519_provider.c  platforms\windows\stn_sha256.c  platforms\windows\stn_storage_windows.c  platforms\windows\stn_peer_windows.c  platforms\windows\stn_app_windows.c"
 
 cl %CFLAGS% %INCLUDES% %SOURCES% ^
     /Fo"%OBJ_DIR%\\" ^
@@ -211,6 +213,36 @@ echo Running Contract accepted-state qualification test...
 if errorlevel 1 goto test_fail
 echo.
 echo CONTRACT STATE TEST SUCCESSFUL
+exit /b 0
+
+
+:test_contract_snapshot
+set "TEST_TARGET=%BUILD_DIR%\test-contract-snapshot.exe"
+echo.
+echo STN Chain Contract snapshot qualification test
+echo Compiler: Microsoft cl.exe
+echo Target:   %TEST_TARGET%
+echo.
+cl %CFLAGS% %INCLUDES% /DSTN_CONTRACT_SNAPSHOT_TEST_MAIN ^
+    tests\test_contract_snapshot.c ^
+    src\stn_contract_snapshot.c ^
+    src\stn_contract_state.c ^
+    src\stn_contract_consensus.c ^
+    src\stn_contract_lineage.c ^
+    src\stn_contract.c ^
+    src\stn_address.c ^
+    src\stn_authority.c ^
+    src\stn_identity.c ^
+    src\crypto\ed25519_donna\ed25519_provider.c ^
+    platforms\windows\stn_sha256.c ^
+    /Fo"%OBJ_DIR%\\" /Fe"%TEST_TARGET%" /link /INCREMENTAL:NO bcrypt.lib
+if errorlevel 1 goto fail
+echo.
+echo Running Contract snapshot qualification test...
+"%TEST_TARGET%"
+if errorlevel 1 goto test_fail
+echo.
+echo CONTRACT SNAPSHOT TEST SUCCESSFUL
 exit /b 0
 
 :test_fail
