f48392edbcc965d31f747e7eea2386aa6ebddef5
Stn-Labz, LLC. <pmei@stn-labz.com>
2026-09-24 02:32:51 -0700

    Link contract consensus into Windows issuance binding test

--- build.cmd
+++ build.cmd
@@ -469,6 +469,7 @@ cl %CFLAGS% %INCLUDES% /DSTN_ISSUANCE_BINDING_TEST_MAIN ^
     src\stn_address.c ^
     src\stn_contract_lineage.c ^
     src\stn_contract_state.c ^
+    src\stn_contract_consensus.c ^
     src\stn_sentinel_intelligence.c ^
     src\stn_contract_snapshot.c ^
     src\stn_contract_transaction.c ^
