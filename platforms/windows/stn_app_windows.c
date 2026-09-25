2ee7c6bd3674746f0dc242a4939c13b1e7ffd2a9
Stn-Labz, LLC. <pmei@stn-labz.com>
2026-09-24 22:10:03 -0700

    Expose Windows address resolution declarations

--- platforms/windows/stn_app_windows.c
+++ platforms/windows/stn_app_windows.c
@@ -5,6 +5,11 @@
 #include "stn_mining.h"
 #include "stn_sha256.h"
 #include "../../src/stn_wire_internal.h"
+#ifndef _WIN32_WINNT
+#define _WIN32_WINNT 0x0600
+#endif
+#include <winsock2.h>
+#include <ws2tcpip.h>
 #include <windows.h>
 #include <stdio.h>
 #include <stdlib.h>
